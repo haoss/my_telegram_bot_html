2e424d06a9d059b24e291ad946cf0d99d3434260
Khrystoiev Dmytro <haossut@gmail.com>
2022-09-09 15:46:25 +0300

    промежуточный коммит

--- dist/js/common.js
+++ dist/js/common.js
@@ -3,6 +3,6 @@
 // Document ready
 $(document).on('ready', function(){
 
-  $('#j_test_01').on('click', function(){ $('#appButtonStatus').toggle(); console.log('click'); })
+  $('#j_test_01').on('click', function(){ $('#appButtonStatus').toggle(); })
 
 });
