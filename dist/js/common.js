a2cabab98716129142b783ddffd73e4a7b3518f0
Khrystoiev Dmytro <haossut@gmail.com>
2022-09-12 14:33:08 +0300

    промежуточный коммит

--- dist/js/common.js
+++ dist/js/common.js
@@ -5,7 +5,9 @@ $(document).on('ready', function(){
 
   $('#j_test_01').on('click', function(){ $('#appButtonStatus').toggle(); })
   $('#j_test_02').on('click', function(){ $('#youPharmacyLine').show(); $('#appModal1').show(); })
-  $('#j_test_04').on('click', function(){ $('#appModal1').show(); })
   $('#j_test_03').on('click', function(){ $('#appModal1').toggle(); })
+  $('#j_test_04').on('click', function(){ $('#appModal1').show(); })
+  $('#j_test_05').on('click', function(){ $('#appModal2').show(); })
+  $('#j_test_06').on('click', function(){ $('#appModal2').hide(); })
 
 });
