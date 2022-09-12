ac189774e9801f3a21d5a2374bce705143a38c74
Khrystoiev Dmytro <haossut@gmail.com>
2022-09-12 12:09:49 +0300

    fix

--- dist/js/common.js
+++ dist/js/common.js
@@ -5,6 +5,7 @@ $(document).on('ready', function(){
 
   $('#j_test_01').on('click', function(){ $('#appButtonStatus').toggle(); })
   $('#j_test_02').on('click', function(){ $('#youPharmacyLine').show(); $('#appModal1').show(); })
+  $('#j_test_04').on('click', function(){ $('#appModal1').show(); })
   $('#j_test_03').on('click', function(){ $('#appModal1').toggle(); })
 
 });
