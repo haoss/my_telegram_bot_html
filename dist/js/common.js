ebec5986fc7256e4cc137e029b6dc115da980e04
Khrystoiev Dmytro <haossut@gmail.com>
2022-09-15 17:58:19 +0300

    промежуточный коммит

--- dist/js/common.js
+++ dist/js/common.js
@@ -5,16 +5,16 @@ $(document).on('ready', function(){
 
   $('#j_test_01').on('click', function(){ $('#appButtonStatus').toggle(); })
   $('#j_test_02').on('click', function(){ $('#youPharmacyLine').show(); $('#appModal1').show(); })
-  $('#j_test_03').on('click', function(){ $('#appModal1').toggle(); })
+  $('.j-app-modal-close-03').on('click', function(){ $(this).parents('.app-modal').hide(); })
   $('#j_test_04').on('click', function(){ $('#appModal1').show(); })
   $('#j_test_05').on('click', function(){ $('#appModal2').show(); })
   $('#j_test_09').on('click', function(){ $('#appModal3').show(); })
   $('#j_test_11').on('click', function(){ $('#appModal4').show(); })
   $('#j_test_12').on('click', function(){ $('#appModal5').show(); })
-  $('#j_test_06').on('click', function(){ $('#appModal2').hide(); })
-  $('#j_test_08').on('click', function(){ $('#appModal3').hide(); })
-  $('#j_test_10').on('click', function(){ $('#appModal4').hide(); })
-  $('#j_test_13').on('click', function(){ $('#appModal5').hide(); })
+  // $('#j_test_06').on('click', function(){ $('#appModal2').hide(); })
+  // $('#j_test_08').on('click', function(){ $('#appModal3').hide(); })
+  // $('#j_test_10').on('click', function(){ $('#appModal4').hide(); })
+  // $('#j_test_13').on('click', function(){ $('#appModal5').hide(); })
   
   $('#j_test_07').on('click', function(){ $('#section1').hide(); $('#section2').show(); $('#j_test_14').focus(); })
   
