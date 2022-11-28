2374362f0b69e9b9eb93d774a81426878c2aea5c
Dmitriy Khristoev <haossut@gmail.com>
2022-11-28 19:42:45 +0200

    fix

--- dist/js/common.js
+++ dist/js/common.js
@@ -4,9 +4,9 @@
 $(document).on('ready', function(){
 
   $('#j_test_01').on('click', function(){ $('#appButtonStatus').toggle(); $('#checkoutButtonStatus').hide(); })
-  $('#j_test_02').on('click', function(){ $('#youPharmacyLine').show(); $('#appModal1').show(); $('.j_test_56').show(); })
+  $('.j_test_02').on('click', function(){ $('#youPharmacyLine').show(); $('#appModal1').show(); $('.j_test_56').show(); })
   $('#j_test_04').on('click', function(){ $('#appModal1').show(); })
-  $('#j_test_05').on('click', function(){ $('#appModal2').show(); })
+  $('.j_test_05').on('click', function(){ $('#appModal2').show(); })
   $('#j_test_09').on('click', function(){ $('#appModal3').show(); })
   $('#j_test_11').on('click', function(){ $('#appModal4').show(); })
   $('#j_test_12').on('click', function(){ $('#appModal5').show(); })
@@ -26,9 +26,9 @@ $(document).on('ready', function(){
   $('#j_test_14').on('click', function(){ $(this).val('Рас').next('.search2__btn-microphone').hide().next('.search2__btn-barcode').hide().next('.search2__btn-close').show(); $('#searchListItem1').hide(); $('#searchListItem2').hide(); $('#searchListItem3').hide(); $('#searchListItem4').show(); $('#searchListItem5').show(); $('#searchListHead').hide(); })
   $('#j_test_15').on('click', function(){ $(this).hide().prev('.search2__btn-barcode').show().prev('.search2__btn-microphone').show().prev().val('').focus(); $('#searchListItem1').show(); $('#searchListItem2').show(); $('#searchListItem3').show(); $('#searchListItem4').hide(); $('#searchListItem5').hide(); $('#searchListHead').show(); $('#section1').show(); $('#section2').hide(); setTimeout(function(){ swiper2 = new Swiper(".j-main-slider", swiper2options); }, 400); })
 
-  $('#j_test_16').on('click', function(){ $('#section2').hide(); $('#section4').show(); })
-  $('#j_test_19').on('click', function(){ $('#section1').hide(); $('#section3').show(); })
-  $('#j_test_17').on('click', function(){ $('#catalog2').hide(); $('#catalog1').show(); $('#j_test_18').removeClass('active'); $(this).addClass('active'); })
+  $('#j_test_16').on('click', function(){ $('.section').hide(); $('#section4').show(); })
+  $('.j_test_19').on('click', function(){ $('.section').hide(); $('#section3').show(); })
+  $('#j_test_17').on('click', function(){ $('.section').hide(); $('#catalog1').show(); $('#j_test_18').removeClass('active'); $(this).addClass('active'); })
   $('#j_test_18').on('click', function(){ $('#catalog1').hide(); $('#catalog2').show(); $('#j_test_17').removeClass('active'); $(this).addClass('active'); })
 
   $('#j_test_20').on('click', function(){ $('#catalog4').hide(); $('#catalog3').show(); $('#j_test_21').removeClass('active'); $(this).addClass('active'); })
@@ -57,7 +57,7 @@ $(document).on('ready', function(){
   $('.j_test_40').on('click', function(){ $(this).hide(); })
   $('.j_test_42').on('click', function(){ $(this).prev().removeClass('--more'); $(this).hide(); })
   $('.j_test_43').on('click', function(){ $(this).parents('.app-modal').hide(); });
-  $('#j_test_44').on('click', function(){ $('#section1').hide(); $('#section8').show(); });
+  $('.j_test_44').on('click', function(){ $('.section').hide(); $('#section8').show(); });
   $('.j_test_45').on('click', function(e){
     e.stopPropagation();
     var _this = $(this);
@@ -96,6 +96,21 @@ $(document).on('ready', function(){
     $(this).parent().find('.btn').addClass('--outline');
     $(this).removeClass('--outline');
   })
+  $('.j_test_58').on('click', function(){
+    $(this).hide().parent().find('.j_test_59').show();
+    $(this).parent().find('.menu__wrapper').show();
+  });
+  $('.j_test_59').on('click', function(){
+    $(this).hide().parent().find('.j_test_58').show();
+    $(this).parent().find('.menu__wrapper').hide();
+  });
+  $('.j_test_60').on('click', function(){ 
+    $('.section').hide(); 
+    $('#section1').show(); 
+    $('.menu__wrapper').hide();
+    $('.j_test_58').show();
+    $('.j_test_59').hide();
+  });
 
   $(document).on('click', '.collapsible-heading', function(e){
     e.preventDefault();
